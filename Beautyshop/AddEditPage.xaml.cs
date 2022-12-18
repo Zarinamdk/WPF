fc247494a33d89b7398eb7d07f71ec7a6f2ef60a
Zarinamdk <reizar2004@mail.ru>
2022-12-18 21:09:52 +0300

    WPF

---
+++ Beautyshop/AddEditPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Beautyshop
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private ID_Мастера _currentMaster = new ID_Мастера();
+        public AddEditPage(ID_Мастера selectedMaster)
+        {
+            if (selectedMaster != null)
+                _currentMaster = selectedMaster;
+            InitializeComponent();
+            DataContext = _currentMaster;
+            ComboKategories.ItemsSource = beautyshopEntities.GetContext().ID_Категории.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentMaster.Фамилия))
+                errors.AppendLine("Укажите фамилию");
+            if (string.IsNullOrWhiteSpace(_currentMaster.Имя))
+                errors.AppendLine("Укажите имя");
+            if (string.IsNullOrWhiteSpace(_currentMaster.Отчество))
+                errors.AppendLine("Укажите отчество");
+            if (string.IsNullOrWhiteSpace(Convert.ToString(_currentMaster.Возраст)))
+                errors.AppendLine("Укажите возраст от 18 до 60");
+            if (Convert.ToString(_currentMaster.Категория) == null)
+                errors.AppendLine("Выберите категорию");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentMaster.ID_мастера1 == 0)
+                beautyshopEntities.GetContext().ID_Мастера.Add(_currentMaster);
+            try
+            {
+                beautyshopEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            
+        }
+    }
+}
---
+++ Beautyshop/NuvPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Beautyshop
+{
+    /// <summary>
+    /// Логика взаимодействия для NuvPage.xaml
+    /// </summary>
+    public partial class NuvPage : Page
+    {
+        public NuvPage()
+        {
+            InitializeComponent();
+            DGridMaster.Items.Clear();
+            //DGridMaster.ItemsSource = beautyshopEntities.GetContext().ID_Мастера.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+      
+        }
+
+        private void BthEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as ID_Мастера));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var mastersForRemoving = DGridMaster.SelectedItems.Cast<ID_Мастера>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {mastersForRemoving.Count()} элемента?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    beautyshopEntities.GetContext().ID_Мастера.RemoveRange(mastersForRemoving);
+                    beautyshopEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+
+                    DGridMaster.ItemsSource = beautyshopEntities.GetContext().ID_Мастера.ToList();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                beautyshopEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridMaster.ItemsSource = beautyshopEntities.GetContext().ID_Мастера.ToList();
+            }
+        }
+    }
+}
